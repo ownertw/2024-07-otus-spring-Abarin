b00f5f0939abe0f2fda847a6145097b883e16b6c
owner_va <owner_va@>
2024-09-16 00:11:32 +0300

    homework hw07

---
+++ hw07/src/main/java/ru/otus/hw/repositories/AuthorRepository.java
@@ -0,0 +1,12 @@
+package ru.otus.hw.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.models.Author;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
